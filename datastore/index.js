de0c59d9931ae70ab81790623c35f0e68fd25794
kage1414 <kylejohnson92294@gmail.com>
2021-02-26 14:39:24 -0500

    Complete deleting a todo

--- datastore/index.js
+++ datastore/index.js
@@ -3,15 +3,12 @@ const path = require('path');
 const _ = require('underscore');
 const counter = require('./counter');
 
-var items = {};
-
 // Public API - Fix these CRUD functions ///////////////////////////////////////
 
 exports.create = (text, callback) => {
   counter.getNextUniqueId((err, id) => {
     let newFileName = id + '.txt';
     let newFilePath = path.join(exports.dataDir, newFileName);
-    items[id] = text;
     fs.writeFile(newFilePath, text, (err) => {
       if (err) {
         console.log('Error Creating Todo : ', err);
@@ -71,14 +68,24 @@ exports.update = (id, text, callback) => {
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  let newFileName = id + '.txt';
+  let newFilePath = path.join(exports.dataDir, newFileName);
+
+  fs.rm(newFilePath, (err) => {
+    if (err) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      callback();
+    }
+  });
+  // var item = items[id];
+  // delete items[id];
+  // if (!item) {
+  //   // report an error if item not found
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   callback();
+  // }
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
